9e380b6cf9dc53e21be0dbbc0e89b4fecb17cb64
Sergio Ibañez <sergio_hd_sony@hotmail.com>
2019-03-10 17:48:24 +0100

    Enunciado actualizado

--- Alquiler.py
+++ Alquiler.py
@@ -53,7 +53,7 @@ with open("alquiler de coches.json") as fichero:
 while True:
     print ('''Menu:
         1. Listar información: Listar nombre de todos los sitios en los que puedan alquilarse coches y su localizacion.
-        2. Contar Informacion: Mostrar sitios de alquiler y contar cuantos de ellos cuentan con foto y pagina web.
+        2. Contar Informacion: Mostrar sitios de alquiler y contar cuantos de ellos cuentan con pagina web.
         3. Buscar o filtrar: Mostrar sitios en los que se pueda alquilar coches para congresos.
         4. Busqueda relacionada: Pedir por teclado un horario y mostrar que sitios estan abiertos.
         5. Ejercicio libre: Mostrar localizacion de los sitios de alquiler usando tanto las coordenadas como openstreetmap.
